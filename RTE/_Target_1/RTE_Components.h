5849349c9c28d6bf05f112eb84a0c21f9780c951
mohsen <ttlcmosic@gmail.com>
2024-10-30 14:16:46 +0330

    -marahele erase va write flash anjam shod
    -resete barnameh moshkel darad -->1403/08/09

---
+++ RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,23 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'boot' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "LPC177x_8x.h"
+
+/*  Keil::Device:Startup:1.0.0 */
+#define RTE_DEVICE_STARTUP_LPC17XX      /* Device Startup for NXP17XX */
+
+
+#endif /* RTE_COMPONENTS_H */
